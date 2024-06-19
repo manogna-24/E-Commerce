173f00b2ecd1e4de6ee315541ff3005c5e90edbd
Manogna Karapakula <manognak249@gmail.com>
2024-06-19 07:45:22 +0530

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,4 +3,5 @@ const router = express.Router()
 const cartAPI=require('../apis/cartApi')
 router.get('/cartfetch',cartAPI.cart_all)
 router.post('/cartInsert',cartAPI.insert_cart)
+router.delete('/cartDelete',cartAPI.del_cart)
 module.exports=router
