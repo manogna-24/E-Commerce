c4ba3781e48a11b11fe6df10ea0edf1dbf4d6b12
Manogna Karapakula <manognak249@gmail.com>
2024-06-19 07:44:58 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -5,6 +5,6 @@ const userSchema= new mongoose.Schema({
     u_pwd: String,
     u_email: String,
     u_addr: String,
-    u_contact: Number
+    u_contact: String
 })
 module.exports=mongoose.model("User", userSchema)
